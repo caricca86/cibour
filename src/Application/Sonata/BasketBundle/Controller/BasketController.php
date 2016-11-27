b96afc866a8e45487c1a1f3a3a472b15976eb9a8
caricca86 <riccardo.cartei7@gmail.com>
2016-11-27 20:34:18 +0100

    controllo piva cf e layout

--- src/Application/Sonata/BasketBundle/Controller/BasketController.php
+++ src/Application/Sonata/BasketBundle/Controller/BasketController.php
@@ -11,6 +11,7 @@
 
 namespace Application\Sonata\BasketBundle\Controller;
 
+use Faker\Provider\cs_CZ\DateTime;
 use Sonata\Component\Delivery\UndeliverableCountryException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DomCrawler\Form;
@@ -23,6 +24,9 @@ use Symfony\Component\Intl\Intl;
 use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 use Sonata\Component\Customer\AddressInterface;
+
+use CTI\CibourBundle\Entity\CodiceFiscale\Checker;
+use CTI\CibourBundle\Entity\CodiceFiscale\Subject;
 
 /**
  * This controller manages the Basket operation and most of the order process
@@ -407,11 +411,68 @@ class BasketController extends Controller
                     $address = $form->getData();
                     $address->setType(AddressInterface::TYPE_BILLING);
 
-                    $customer->addAddress($address);
+                    $cf = $address->getCodiceFiscale();
+                    $pIva = $address->getPartitaIva();
 
-                    $this->get('sonata.customer.manager')->save($customer);
+                    if ($this->ControllaCF($cf) && $this->controllaPIVA($pIva)) {
+                        $anno = substr($cf, 6, 2);
+                        $tabellamesi = array(
+                            "A" => "01",
+                            "B" => "02",
+                            "C" => "03",
+                            "D" => "04",
+                            "E" => "05",
+                            "H" => "06",
+                            "L" => "07",
+                            "M" => "08",
+                            "P" => "09",
+                            "R" => "10",
+                            "S" => "11",
+                            "T" => "12"
+                        );
+                        $mese = $tabellamesi[strtoupper(substr($cf, 8, 1))];
+                        $giorno = substr($cf, 9, 2);
 
-                    $this->get('session')->getFlashBag()->add('sonata_customer_success', 'address_add_success');
+                        $diff = date_diff(date_create(), date_create("$anno-$mese-$giorno"));
+
+                        if ($diff->y < 18) {
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', "L'acquisto è permesso solo a utenti con età maggiore di 18 anni");
+
+                            $this->get('session')->set('sonata_address_redirect', $this->generateUrl('sonata_basket_payment_address'));
+
+                            $this->get('sonata.seo.page')->setTitle($this->get('translator')->trans('basket_payment_title', array(), "SonataBasketBundle"));
+
+                            return $this->render($template, array(
+                                'form'      => $form->createView(),
+                                'addresses' => $addresses
+                            ));
+                        } else {
+                            $customer->addAddress($address);
+
+                            $this->get('sonata.customer.manager')->save($customer);
+
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', 'address_add_success');
+                        }
+                    } else {
+                        if (!$this->ControllaCF($cf)){
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', 'Codice fiscale errato');
+                        }
+
+                        if (!$this->controllaPIVA($pIva)){
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', 'Partita iva errata');
+                        }
+
+                        $this->get('session')->set('sonata_address_redirect', $this->generateUrl('sonata_basket_payment_address'));
+
+                        $this->get('sonata.seo.page')->setTitle($this->get('translator')->trans('basket_payment_title', array(), "SonataBasketBundle"));
+
+                        return $this->render($template, array(
+                            'form'      => $form->createView(),
+                            'addresses' => $addresses
+                        ));
+                    }
+
+
                 }
 
                 $basket->setCustomer($customer);
@@ -484,5 +545,50 @@ class BasketController extends Controller
             'basket'    => $basket,
             'tac_error' => $this->get('request')->getMethod() == 'POST'
         ));
+    }
+
+    function ControllaCF($cf)
+    {
+        if( $cf === '' )  return false;
+        if( strlen($cf) != 16 )
+            return false;
+        $cf = strtoupper($cf);
+        if( preg_match("/^[A-Z0-9]+\$/", $cf) != 1 ){
+            return false;
+        }
+
+        return true;
+    }
+
+    function controllaPIVA($variabile){
+
+        if($variabile=='')
+            return false;
+
+        //la p.iva deve essere lunga 11 caratteri
+        if(strlen($variabile)!=11)
+            return false;
+
+        //la p.iva deve avere solo cifre
+        if(!ereg("^[0-9]+$", $variabile))
+            return false;
+
+        $primo=0;
+        for($i=0; $i<=9; $i+=2)
+            $primo+= ord($variabile[$i])-ord('0');
+
+        for($i=1; $i<=9; $i+=2 ){
+            $secondo=2*( ord($variabile[$i])-ord('0') );
+
+            if($secondo>9)
+                $secondo=$secondo-9;
+            $primo+=$secondo;
+
+        }
+        if( (10-$primo%10)%10 != ord($variabile[10])-ord('0') )
+            return false;
+
+        return true;
+
     }
 }
--- src/Application/Sonata/CustomerBundle/Controller/CustomerController.php
+++ src/Application/Sonata/CustomerBundle/Controller/CustomerController.php
@@ -164,6 +164,66 @@ class CustomerController extends Controller
             if ($form->isValid()) {
                 $address = $form->getData();
 
+                if ($address->getType() == AddressInterface::TYPE_BILLING){
+                    $cf = $address->getCodiceFiscale();
+                    $pIva = $address->getPartitaIva();
+
+                    if ($this->ControllaCF($cf) && $this->controllaPIVA($pIva)) {
+                        $anno = substr($cf, 6, 2);
+                        $tabellamesi = array(
+                            "A" => "01",
+                            "B" => "02",
+                            "C" => "03",
+                            "D" => "04",
+                            "E" => "05",
+                            "H" => "06",
+                            "L" => "07",
+                            "M" => "08",
+                            "P" => "09",
+                            "R" => "10",
+                            "S" => "11",
+                            "T" => "12"
+                        );
+                        $mese = $tabellamesi[strtoupper(substr($cf, 8, 1))];
+                        $giorno = substr($cf, 9, 2);
+
+                        $diff = date_diff(date_create(), date_create("$anno-$mese-$giorno"));
+
+                        if ($diff->y < 18) {
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', "L'acquisto è permesso solo a utenti con età maggiore di 18 anni");
+
+                            return $this->render($template, array(
+                                'form'               => $form->createView(),
+                                'breadcrumb_context' => 'customer_address',
+                            ));
+
+                        } else {
+                            $customer->addAddress($address);
+
+                            $this->getCustomerManager()->save($customer);
+
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', $id ? 'address_edit_success' : 'address_add_success');
+
+                            $url = $this->get('session')->get('sonata_address_redirect', $this->generateUrl('sonata_customer_addresses'));
+
+                            return new RedirectResponse($url);
+                        }
+                    } else {
+
+                        if (!$this->ControllaCF($cf)){
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', 'Codice fiscale errato');
+                        }
+
+                        if (!$this->controllaPIVA($pIva)){
+                            $this->get('session')->getFlashBag()->add('sonata_customer_success', 'Partita iva errata');
+                        }
+
+                        return $this->render($template, array(
+                            'form'               => $form->createView(),
+                            'breadcrumb_context' => 'customer_address',
+                        ));
+                    }
+                }
                 $customer->addAddress($address);
 
                 $this->getCustomerManager()->save($customer);
@@ -221,5 +281,50 @@ class CustomerController extends Controller
     protected function getCustomerManager()
     {
         return $this->get('sonata.customer.manager');
+    }
+
+    function ControllaCF($cf)
+    {
+        if( $cf === '' )  return false;
+        if( strlen($cf) != 16 )
+            return false;
+        $cf = strtoupper($cf);
+        if( preg_match("/^[A-Z0-9]+\$/", $cf) != 1 ){
+            return false;
+        }
+
+        return true;
+    }
+
+    function controllaPIVA($variabile){
+
+        if($variabile=='')
+            return false;
+
+        //la p.iva deve essere lunga 11 caratteri
+        if(strlen($variabile)!=11)
+            return false;
+
+        //la p.iva deve avere solo cifre
+        if(!ereg("^[0-9]+$", $variabile))
+            return false;
+
+        $primo=0;
+        for($i=0; $i<=9; $i+=2)
+            $primo+= ord($variabile[$i])-ord('0');
+
+        for($i=1; $i<=9; $i+=2 ){
+            $secondo=2*( ord($variabile[$i])-ord('0') );
+
+            if($secondo>9)
+                $secondo=$secondo-9;
+            $primo+=$secondo;
+
+        }
+        if( (10-$primo%10)%10 != ord($variabile[10])-ord('0') )
+            return false;
+
+        return true;
+
     }
 }
